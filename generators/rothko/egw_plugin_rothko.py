5e232814b374dedffd01823cbbbf3ce499dd3dfb
Sven Hessenmüller <sven.hessenmueller@gmail.com>
2020-03-06 23:32:39 +0100

    adds color-palette support to generator rothko

--- generators/rothko/egw_plugin_rothko.py
+++ generators/rothko/egw_plugin_rothko.py
@@ -22,7 +22,7 @@ log.addHandler(handler)
 
 meta = {
     "name" : "rothko",
-    "version" : "0.1.1",
+    "version" : "0.1.2",
     "description" : "adaption of [RothkoBot](https://github.com/ZacharyDavis/RothkoBot), Copyright (c) 2017 Zachary Davis",
 }
 
@@ -31,7 +31,13 @@ class Rothko(EGWPluginGenerator):
         super().__init__(**meta)
         # defining mandatory kwargs (addionals to the mandatory of the base-class)
         self.add_plugin_kwargs = {
-                                    'seed' : None
+                                    'seed' : None,
+                                    'palette' : 'colors.txt',
+                                    'color1' : None,
+                                    'color2' : None,
+                                    'color3' : None,
+                                    'color4' : None,
+
                                  }
         self._define_mandatory_kwargs(self, **self.add_plugin_kwargs)
         self.kwargs = kwargs
@@ -48,45 +54,84 @@ assert isinstance(generator,EGWPluginGenerator)
 
 
 # here we go
-def _do_rothko(color_file='colors.txt', seed = None, size=(418,518), \
-               img_mode='RGBA', bg_color = (255,255,255,255)):
+def _do_rothko(palette='colors.txt', seed = None, size=(418,518), \
+               img_mode='RGBA', color1 = None, \
+               color2 = None, color3 = None, color4 = None):
+    """
+    color_palette:
+        plaintextfile with a palette of colors as strings in the format
+        described below
+
+    color1..4:
+        colors are strings in the following format: '<name>,#<rr><gg><bb>'
+
+    example:
+        color1='pebble gray,#e0e4cc'
+    """
+
     if not seed:
         seed = random.randrange(sys.maxsize)
     log.info("seed: {}".format(seed))
     random.seed(seed)
-    if not bg_color or not isinstance(bg_color,tuple) or len(bg_color) !=4:
-        bg_color = (255,255,255,255)
-    img = Image.new(img_mode, size, bg_color)
+    if color1:
+        if isinstance(color1,str):
+            color1 = color1.split(",")
+        else:
+            color1 = None
+    if color2: color2 = color2.split(",")
+    if color3: color3 = color3.split(",")
+    if color4: color4 = color4.split(",")
+    img = Image.new(img_mode, size,(255,255,255,255))
     draw = ImageDraw.Draw(img)
 
     # read colornames and rgb-string from file
-    f_in = open("{}/{}".format(os.path.dirname(os.path.realpath(__file__)),color_file),'r')
+    try:
+        f_in = open(palette,'r')
+    except:
+        f_in = open("{}/{}".format( \
+            os.path.dirname(os.path.realpath(__file__)),palette),'r')
     colors = f_in.read().splitlines()
     f_in.close()
     four_colors = False
-    color1 = colors[random.randint(0,len(colors)-1)].split(",")  # background color and name
-    color2 = colors[random.randint(0,len(colors)-1)].split(",")  # first rectangle color and name
-    color3 = colors[random.randint(0,len(colors)-1)].split(",")  # second rectangle color and name
-    color4 = colors[random.randint(0,len(colors)-1)].split(",")  # third rectangle color and name
+    if not color1:
+        color1 = colors[random.randint(0,len(colors)-1)].split(",")  # background color and name
+    else:
+        # dummy for getting same result for same seed
+        random.randint(0,len(colors)-1)
+    if not color2:
+        color2 = colors[random.randint(0,len(colors)-1)].split(",")  # first rectangle color and name
+    else:
+        # dummy for getting same result for same seed
+        random.randint(0,len(colors)-1)
+    if not color3:
+        color3 = colors[random.randint(0,len(colors)-1)].split(",")  # second rectangle color and name
+    else:
+        # dummy for getting same result for same seed
+        random.randint(0,len(colors)-1)
+    if not color4:
+        color4 = colors[random.randint(0,len(colors)-1)].split(",")  # third rectangle color and name
+    else:
+        # dummy for getting same result for same seed
+        random.randint(0,len(colors)-1)
     # decode hex color values to (R,G,B) format
-    color_str1 = tuple(int(color1[1].lstrip('#')[i:i+2], 16) for i in (0, 2, 4))
-    color_str2 = tuple(int(color2[1].lstrip('#')[i:i+2], 16) for i in (0, 2, 4))
-    color_str3 = tuple(int(color3[1].lstrip('#')[i:i+2], 16) for i in (0, 2, 4))
-    color_str4 = tuple(int(color4[1].lstrip('#')[i:i+2], 16) for i in (0, 2, 4))
-    log.info("color1 {} = {}".format(color1,color_str1))
-    log.info("color2 {} = {}".format(color2,color_str2))
-    log.info("color3 {} = {}".format(color3,color_str3))
-    log.info("color4 {} = {}".format(color4,color_str4))
+    color1_rgba = tuple(int(color1[1].lstrip('#')[i:i+2], 16) for i in (0, 2, 4))
+    color2_rgba = tuple(int(color2[1].lstrip('#')[i:i+2], 16) for i in (0, 2, 4))
+    color3_rgba = tuple(int(color3[1].lstrip('#')[i:i+2], 16) for i in (0, 2, 4))
+    color4_rgba = tuple(int(color4[1].lstrip('#')[i:i+2], 16) for i in (0, 2, 4))
+    log.info("color1 {} = {}".format(color1,color1_rgba))
+    log.info("color2 {} = {}".format(color2,color2_rgba))
+    log.info("color3 {} = {}".format(color3,color3_rgba))
+    log.info("color4 {} = {}".format(color4,color4_rgba))
 
     w,h = size[0],size[1]
     length1 = random.randint(math.floor((h / 100) * 1.94),math.floor((h / 100) * 54.0))  # Determine lengths of each rectangle
     length2 = random.randint(0,(math.floor((h / 100) * 92.8)-length1))
     length3 = math.floor((h / 100) * 89.4)-(length2+length1)
-    draw.rectangle([0, 0, w, h], fill=color_str1, outline=color_str1)
-    draw.rectangle([math.floor((h / 100) * 3.9),math.floor((h / 100) * 3.9), w-math.floor((h / 100) * 3.9), length1+math.floor((h / 100) * 3.9)], fill=color_str2, outline=color_str2)
-    draw.rectangle([math.floor((h / 100) * 3.9), length1+math.floor((h / 100) * 5.9), w-math.floor((h / 100) * 3.9), length2+length1+math.floor((h / 100) * 5.9)], fill=color_str3, outline=color_str3)
+    draw.rectangle([0, 0, w, h], fill=color1_rgba, outline=color1_rgba)
+    draw.rectangle([math.floor((h / 100) * 3.9),math.floor((h / 100) * 3.9), w-math.floor((h / 100) * 3.9), length1+math.floor((h / 100) * 3.9)], fill=color2_rgba, outline=color2_rgba)
+    draw.rectangle([math.floor((h / 100) * 3.9), length1+math.floor((h / 100) * 5.9), w-math.floor((h / 100) * 3.9), length2+length1+math.floor((h / 100) * 5.9)], fill=color3_rgba, outline=color3_rgba)
     if length3 > 0:
-        draw.rectangle([math.floor((h / 100) * 3.9), length1+length2+math.floor((h / 100) * 7.9), w-math.floor((h / 100) * 3.9), length1+length2+length3+math.floor((h / 100) * 7.9)], fill=color_str4, outline=color_str4)
+        draw.rectangle([math.floor((h / 100) * 3.9), length1+length2+math.floor((h / 100) * 7.9), w-math.floor((h / 100) * 3.9), length1+length2+length3+math.floor((h / 100) * 7.9)], fill=color4_rgba, outline=color4_rgba)
         four_colors = True
     if four_colors:
         sTitle = "No. " + str(seed) + " (" + color2[0].title() + ", " + color3[0].title() + ", and " + color4[0].title() + " on " + color1[0].title() + ")"
--- generators/rothko/run_test.sh
+++ generators/rothko/run_test.sh
@@ -4,16 +4,17 @@ rm -i ./*png
 FOUT=$(realpath "./egw_plugin_rothko.selftest.png")
 CMD=$(realpath "./egw_plugin_rothko.py")
 $CMD $FOUT || exit 1
-feh $FOUT
+feh -Z -F $FOUT
 
 # No. 1372403442 (Free Orange, Hazel, and Deep Sea Blue on Dark Blue Grey)
+# No. 1551414546 (Pinky Pink, Orange Pink, and Taupe on Pebble Gray)
 # No. 988209721 (Light Blue Grey, Dark Grey Blue, and Blush Pink on Green Blue)
 # ... etc.pp.
-SEEDS=('1372403442' '988209721' '1703988514' '1703988515' '1452541309')
+SEEDS=('839605687' '1372403442' '1551414546' '988209721' '1703988514' '1703988515' '1452541309')
 for seed in "${SEEDS[@]}"
 do
   FOUT=$(realpath "./egw_plugin_rothko.selftest.seed_${seed}.png")
   echo "...creating ${FOUT}"
   $CMD $FOUT $seed || exit 1
 done
-feh ./*png
+feh -Z -F ./*png
